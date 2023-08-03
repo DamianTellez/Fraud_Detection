4f506802ec2fc6abc2da88caf5ac000109208e77
A.D. Téllez <127765518+DamianTellez@users.noreply.github.com>
2023-08-03 21:29:06 +0200

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import preprocessing 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 #=====================================================#
